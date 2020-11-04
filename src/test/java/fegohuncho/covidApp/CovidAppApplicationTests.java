dd7bb9e23d966edd0072e466e7a8cbfba2c8f51e
ofego <edafeo1@gmail.com>
2020-11-05 01:31:32 +1100

    Covid Software

---
+++ src/test/java/fegohuncho/covidApp/CovidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fegohuncho.covidApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
