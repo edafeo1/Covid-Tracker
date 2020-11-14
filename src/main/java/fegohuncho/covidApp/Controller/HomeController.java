17b18af7dd103d7073a3fd63aada4a823e6fa617
ofego <edafeo1@gmail.com>
2020-11-14 23:59:29 +1100

    no message

--- src/main/java/fegohuncho/covidApp/Controller/HomeController.java
+++ src/main/java/fegohuncho/covidApp/Controller/HomeController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -27,5 +29,11 @@ public class HomeController {
           model.addAttribute("totalNewCases", totalNewCases);
 
         return "home";
+    }
+
+    @GetMapping("/stats")
+    @ResponseBody
+    public List<LocationStats> getStats() {
+        return covidDataService.getTotalStats();
     }
 }
